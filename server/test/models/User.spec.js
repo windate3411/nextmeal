4f9508530ac17483a717262e537d92e0fc01c59e
Danny <windate3411@gmail.com>
2019-10-13 15:48:50 +0800

    add User model test

--- server/test/models/User.spec.js
+++ server/test/models/User.spec.js
@@ -25,3 +25,56 @@ describe('test unit', function () {
     expect(1).to.be.equal(1)
   })
 })
+
+const db = require('../../models')
+const UserModel = require('../../models/user')
+
+describe('# UserModel Model', () => {
+
+  before(done => {
+    done()
+  })
+
+  const User = UserModel(sequelize, dataTypes)
+  const user = new User()
+  checkModelName(User)('User')
+
+  context('properties', () => {
+    ;[
+      'name', 'email', 'password'
+    ].forEach(checkPropertyExists(user))
+  });
+  context('action', () => {
+    let data = null
+
+    it('create', (done) => {
+      db.User.create({}).then((user) => {
+        data = user
+        done()
+      })
+    })
+    it('read', (done) => {
+      db.User.findByPk(data.id).then((user) => {
+        expect(data.id).to.be.equal(user.id)
+        done()
+      })
+    })
+    it('update', (done) => {
+      db.User.update({}, { where: { id: data.id } }).then(() => {
+        db.User.findByPk(data.id).then((user) => {
+          expect(data.updatedAt).to.be.not.equal(user.updatedAt)
+          done()
+        })
+      })
+    })
+    it('delete', (done) => {
+      db.User.destroy({ where: { id: data.id } }).then(() => {
+        db.User.findByPk(data.id).then((user) => {
+          expect(user).to.be.equal(null)
+          done()
+        })
+      })
+    })
+  })
+
+})
